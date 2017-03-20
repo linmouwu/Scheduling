68b972161ec9a19a5f17c0f0f1f16269665e2fa2
Akash Ravindranath <akashravindranath@gmail.com>
2017-03-19 22:36:00 -0400

    Authentication Classes Spring with ldap. Modified the classes. Added test ldif file to test auth

--- src/main/java/cmu/heinz/util/WebSecurityConfig.java
+++ src/main/java/cmu/heinz/util/WebSecurityConfig.java
@@ -1,18 +1,17 @@
-package util;
+package cmu.heinz.util;
+import java.util.Arrays;
 
-import cmu.heinz.util.Authenticate;
-import javax.naming.directory.DirContext;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.encoding.LdapShaPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 
 
 /**
@@ -23,41 +22,41 @@ import org.springframework.security.core.AuthenticationException;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements AuthenticationProvider {
+    
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/", "/login").permitAll()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .successForwardUrl("/home")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
-        http.csrf().disable();
-    }
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+                                .antMatchers("/", "/login").permitAll()
+				.anyRequest().fullyAuthenticated()
+				.and()
+                        .formLogin()
+                        .loginPage("/login")
+                        .successForwardUrl("/home")
+                        ;
+                http.csrf().disable();
+	}
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        
-        auth
-                .inMemoryAuthentication()
-                .withUser("001").password("123").roles("USER");
-        auth
-                .inMemoryAuthentication()
-                .withUser("002").password("123").roles("USER");
-    }
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.ldapAuthentication()
+				.userDnPatterns("uid={0},ou=people")
+				.groupSearchBase("ou=groups")
+				.contextSource(contextSource())
+				.passwordCompare()
+					.passwordEncoder(new LdapShaPasswordEncoder())
+					.passwordAttribute("userPassword");
+	}
+
+	@Bean
+	public DefaultSpringSecurityContextSource contextSource() {
+		return  new DefaultSpringSecurityContextSource(Arrays.asList("ldap://localhost:8389/"), "dc=springframework,dc=org");
+	}
 
     @Override
     public Authentication authenticate(Authentication a) throws AuthenticationException {
-        DirContext ctx = Authenticate.authenticateUser(a.getName(), a.getCredentials().toString());
-        if(ctx == null){
-            throw new BadCredentialsException("PLease check your credentials");
-        }
-        
-        return new UsernamePasswordAuthenticationToken(a.getName(), a.getCredentials().toString());
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
