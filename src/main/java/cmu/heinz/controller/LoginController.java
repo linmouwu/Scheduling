2d54336101e3274cb25fc81703f83b5aa0151f3e
Mouwu Lin <mouwu.lin@gmail.com>
2017-03-20 17:31:53 -0400

    Merge Akash, Nan merged.

--- src/main/java/cmu/heinz/controller/LoginController.java
+++ src/main/java/cmu/heinz/controller/LoginController.java
@@ -1,11 +1,12 @@
 package cmu.heinz.controller;
 
-import java.util.Optional;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 /**
  * @author Mouwu Lin
@@ -15,7 +16,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class LoginController {
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    @RequestMapping(value = {"/login", "/"}, method = RequestMethod.GET)
     public ModelAndView getLoginPage(@RequestParam Optional<String> error) {
         return new ModelAndView("login", "error", error);
     }
