cf3aa69168f22bda9fefb5ee0f0ad0ea8e589c93
Mouwu Lin <mouwu.lin@gmail.com>
2017-04-08 11:32:33 -0400

    04/07/2017 Comment added.

--- src/main/java/cmu/heinz/controller/HomeController.java
+++ src/main/java/cmu/heinz/controller/HomeController.java
@@ -51,39 +51,53 @@ public class HomeController {
     @RequestMapping(value = "/home", method = {RequestMethod.POST, RequestMethod.GET})
     public String home(Model model) {
 
+        // User log in evidence.
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         UserDetails userDetails = (UserDetails) principal;
 
         String username = userDetails.getUsername();
 
+        // Fetch user details by username(UID).
         Officer officer = officerRepository.findByUID(username);
 
         int id = officer.getId();
         System.out.println(id);
 
+        // User details.
         String uid = officer.getUid();
+
         String unionID = "1";
+
         String permissionGroup = officer.getPermissionGroup();
+
         System.out.println("the id is " + uid);
+
+        // List all users.
         List<Officer> officerList = (List<Officer>) officerRepository.findAll();
 
+        // List all unions.
         List<Union> unionList = (List<Union>) unionRepository.findAll();
+
+        // List all pending and previous events.
         List<Event> pendingEventList = new ArrayList<Event>();
         List<Event> previousEventList = new ArrayList<Event>();
 
         if (permissionGroup.equals("User")) {
 
             // If the current user is a regular user.
-            pendingEventList = (List<Event>) eventRepository.findByPendingUID(uid);
-            previousEventList = (List<Event>) eventRepository.findByPreviousUID(uid);
+            pendingEventList = eventRepository.findByPendingUID(uid);
+            previousEventList = eventRepository.findByPreviousUID(uid);
+
         } else if (permissionGroup.equals("Administrator")) {
 
             // If the current user  is a administrator.
-            pendingEventList = (List<Event>) eventRepository.findByPendingUnionID(unionID);
-            previousEventList = (List<Event>) eventRepository.findByPreviousUnionID(unionID);
+            pendingEventList = eventRepository.findByPendingUnionID(unionID);
+            previousEventList = eventRepository.findByPreviousUnionID(unionID);
+
         } else if (permissionGroup.equals("Master Administrator")) {
 
+            // If the current user is a master administrator.
             TimeCycle timeCycleActivated = timeCycleRepository.findActivate();
 
             if (timeCycleActivated != null) {
@@ -96,17 +110,17 @@ public class HomeController {
 
             }
 
-        } else if (permissionGroup.equals("Technical Administrator")) {
+        } else if (permissionGroup.equals("Master Technician")) {
 
+            // TODO: if a current user is a master technician
         } else {
             // Other users.
             pendingEventList = new ArrayList<Event>();
             previousEventList = new ArrayList<Event>();
         }
 
+        // Model attributes.
         model.addAttribute("officer", officer);
-
-        System.out.println("PG: " + officer.getPermissionGroup());
 
         model.addAttribute("officerList", officerList);
 
--- src/main/java/cmu/heinz/controller/TimeCycleController.java
+++ src/main/java/cmu/heinz/controller/TimeCycleController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Date;
 
 /**
+ * Controller to manage the time cycle.
+ *
  * @author Mouwu Lin
  * @AndrewID mouwul
  */
@@ -20,19 +22,30 @@ import java.util.Date;
 public class TimeCycleController {
 
 
+    /**
+     * Time cycle repository interface.
+     */
     @Autowired
     public TimeCycleRepository timeCycleRepository;
 
+    /**
+     * Deactivate the current time cycle. Usually there is only one time cycle activated.
+     *
+     * @param id the time cycle to be deactivated.
+     * @return 200 OK.
+     */
     @RequestMapping(value = "/deactivate_event", method = {RequestMethod.PUT, RequestMethod.GET})
     public ResponseEntity deactivateEvent(
             @RequestParam(value = "timeCycleId") Integer id) {
 
         try {
 
+            // Try to deactivate time cycle.
             timeCycleRepository.deactivateById(id);
 
         } catch (Exception e) {
 
+            // If failed, return 400.
             e.printStackTrace();
             return ResponseEntity.badRequest().build();
 
@@ -41,21 +54,32 @@ public class TimeCycleController {
         return ResponseEntity.ok().build();
     }
 
+    /**
+     * Add new time cycle which can be populated to the whole application.
+     *
+     * @param startDate start date of current time cycle
+     * @param endDate   end date of current time cycle
+     * @return 200 OK
+     */
     @RequestMapping(value = "/add_time_cycle", method = {RequestMethod.POST})
     public ResponseEntity addTimeCycle(
             @RequestParam(value = "startDate") @DateTimeFormat(pattern = "MM/dd/yyyy") Date startDate,
             @RequestParam(value = "endDate") @DateTimeFormat(pattern = "MM/dd/yyyy") Date endDate) {
 
+        // New time cycle object.
         TimeCycle timeCycle = new TimeCycle();
 
         timeCycle.setStartDate(startDate);
 
         timeCycle.setEndDate(endDate);
 
+        // A new time cycle can be created only when no time cycle is activated,
+        // thus the new time cycle is automatically activated.
         timeCycle.setActivated(true);
 
         timeCycleRepository.save(timeCycle);
 
+        // 200 OK.
         return ResponseEntity.ok().build();
     }
 
