67ee3b1e2c91e8276ca8a07c970cf9db4d9432e2
Mouwu Lin <mouwu.lin@gmail.com>
2017-05-08 18:14:45 -0400

    05/03/2017 Update.

--- src/main/java/cmu/heinz/model/GroupScheduleRepository.java
+++ src/main/java/cmu/heinz/model/GroupScheduleRepository.java
@@ -14,11 +14,15 @@ import java.util.List;
 public interface GroupScheduleRepository extends CrudRepository<GroupSchedule, String> {
     @Query(value = "select SUM(selected_officer) from GROUP_SCHEDULE where union_id = (:param1) AND schedule_status != 'pending' AND schedule_status != 'declined' AND start_time <= (:param2) AND end_time >= (:param2) AND shift_id = (:param3)", nativeQuery = true)
     Integer findByAllDate(@Param("param1") int unionid, @Param("param2") String date, @Param("param3") int shiftId);
-//@Query(value = "select COUNT(DISTINCT officer_id) from EVENT_REQUEST where union_id = (:param1) AND event_status != 'pending' AND event_status != 'declined' AND start_time <= (:param2) AND end_time >= (:param3)", nativeQuery = true)
+    //@Query(value = "select COUNT(DISTINCT officer_id) from EVENT_REQUEST where union_id = (:param1) AND event_status != 'pending' AND event_status != 'declined' AND start_time <= (:param2) AND end_time >= (:param3)", nativeQuery = true)
 //Integer findByAllDate(@Param("param1") int unionid,@Param("param2") String sd, @Param("param3") String ed);
     @Query(value = "select * from GROUP_SCHEDULE where union_id = (:param1) order by start_time desc", nativeQuery = true)
     List<GroupSchedule> findByUnion(@Param("param1") int unionId);
     @Query(value = "select * from GROUP_SCHEDULE where id = (:param1)", nativeQuery = true)
     GroupSchedule findById(@Param("param1") int id);
+
+    @Query(value = "select * from GROUP_SCHEDULE where union_id = (:param1) AND schedule_status != 'pending' AND schedule_status != 'declined' AND shift_id = (:param2)", nativeQuery = true)
+    List<GroupSchedule> findByAllDateUnionShift(@Param("param1") int unionid, @Param("param2") int shiftId);
+
 
 }
